47b05eec87ba9bebabfba3a426a635bad2271681
jazztinecruz <me@jazztinecruz.com>
2024-10-28 23:02:27 +0800

    feat(api): add route to get item with its count

---
+++ app/api/(database)/(items)/get-item/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import db from "../../db-client";
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const id = parseInt(String(searchParams.get("id")));
+
+  try {
+    const item = await db.item.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        _count: true,
+      },
+    });
+
+    if (!item) {
+      return NextResponse.json({ message: "Item not found" }, { status: 404 });
+    }
+    return NextResponse.json(item, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(error, { status: 500 });
+  }
+};
