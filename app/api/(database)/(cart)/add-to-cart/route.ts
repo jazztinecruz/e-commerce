43318829e0384a37428c906babc9a518fb80a91d
jazztinecruz <me@jazztinecruz.com>
2024-10-28 23:02:52 +0800

    feat(api): add route to add item to cart

---
+++ app/api/(database)/(cart)/add-to-cart/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import db from "../../db-client";
+import { CartItem } from "@prisma/client";
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const { data } = body as { data: CartItem };
+
+  try {
+    const cartItem = await db.cartItem.create({
+      data,
+      include: {
+        item: true,
+        cart: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(cartItem, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(error, { status: 500 });
+  }
+};
