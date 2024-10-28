dd94fe83804c97df0bf4f6684b774233d645354f
jazztinecruz <me@jazztinecruz.com>
2024-10-28 21:01:57 +0800

    feat(database): add Prisma client and user routes

---
+++ app/api/(database)/(users)/get-users/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import db from "../db-client";
+
+export const GET = async () => {
+  try {
+    const users = await db.user.findMany();
+    return NextResponse.json(users, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(error, { status: 500 });
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-const HomePage = () => {
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const HomePage = async () => {
+  const users = await db.user.findMany();
+
+  console.log({ users });
   return <div>HomePage</div>;
 };
 
