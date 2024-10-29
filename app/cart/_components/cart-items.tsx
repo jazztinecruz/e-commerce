cba043f09cb5d053c83fc589b91d25f1f5ddb617
jazztinecruz <me@jazztinecruz.com>
2024-10-29 22:34:09 +0800

    refactor(cart): Add select items to checkout

--- app/cart/_components/cart-items.tsx
+++ app/cart/_components/cart-items.tsx
@@ -1,33 +1,99 @@
 "use client";
 
+import { Button, Checkbox } from "@nextui-org/react";
 import CartItem from "./cart-item";
 import type api from "@/core/api";
+import { ChangeEvent, useState } from "react";
+import { ExtendedCartItem } from "@/core/types/item";
 
 type Props = {
   cartItems: Awaited<ReturnType<typeof api.get.cart.items>>;
 };
 
 const CartItems = ({ cartItems }: Props) => {
+  const [selectedItems, setSelectedItems] = useState<ExtendedCartItem[]>([]);
+
   if (!cartItems) return <div>Cart is empty</div>;
 
+  const handleSelect = (item: ExtendedCartItem) => {
+    setSelectedItems((prev) => {
+      if (prev.includes(item)) {
+        return prev.filter((prevItem) => prevItem !== item);
+      }
+      return [...prev, item];
+    });
+  };
+
+  const handleSelectAll = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setSelectedItems(cartItems.items);
+    } else {
+      setSelectedItems([]);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="flex items-center gap-2">
-        <span className="text-2xl font-bold">Cart</span>
-        <span className="text-default-foreground">
-          ({cartItems.items.length} Items)
-        </span>
-      </h1>
-      <ul>
-        {cartItems.items.map(({ item, quantity }, index) => (
-          <CartItem
-            key={index}
-            cartItem={item}
-            cartId={Number(cartItems.id)}
-            quantity={quantity}
-          />
-        ))}
-      </ul>
+    <div className="grid grid-cols-[2fr,1fr] gap-10">
+      <div className="space-y-4">
+        <h1 className="flex items-center gap-2">
+          <span className="text-2xl font-bold">Cart</span>
+          <span className="text-default-foreground">
+            ({cartItems.items.length} Items)
+          </span>
+        </h1>
+
+        <div className="flex items-center gap-2">
+          <Checkbox onChange={(event) => handleSelectAll(event)} />
+          <span className="text-sm text-default-500">
+            Select All ({cartItems.items.length}) Items
+          </span>
+        </div>
+
+        <ul>
+          {cartItems.items.map((cartItem, index) => (
+            <div key={index} className="flex items-center gap-2">
+              <Checkbox
+                isSelected={selectedItems.includes(cartItem)}
+                onChange={() => handleSelect(cartItem)}
+              />
+              <CartItem
+                cartItem={cartItem.item}
+                cartId={Number(cartItems.id)}
+                quantity={cartItem.quantity}
+              />
+            </div>
+          ))}
+        </ul>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl font-bold">Order Summary</h2>
+        <ul className="mt-4">
+          {selectedItems.map(({ item, quantity }, index) => (
+            <li key={index} className="flex justify-between py-2">
+              <span>
+                {item.name} x {quantity}
+              </span>
+              <span>${(item.price * quantity).toFixed(2)}</span>
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-between mt-4 pt-4 border-t">
+          <span className="font-bold ">Total</span>
+          <span className="font-bold text-xl">
+            PHP
+            {selectedItems
+              .reduce(
+                (total, { item, quantity }) => total + item.price * quantity,
+                0
+              )
+              .toFixed(2)}
+          </span>
+        </div>
+        <Button color="primary" className="w-full uppercase font-bold">
+          Checkout
+        </Button>
+      </div>
     </div>
   );
 };
