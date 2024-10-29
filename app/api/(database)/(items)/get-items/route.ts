0e3e89323b222212ee8c4f9669105c274200ba5f
jazztinecruz <me@jazztinecruz.com>
2024-10-29 17:05:47 +0800

    refactor(api): Simplify get-items route

--- app/api/(database)/(items)/get-items/route.ts
+++ app/api/(database)/(items)/get-items/route.ts
@@ -3,11 +3,7 @@ import { prisma } from "@/prisma";
 
 export const GET = async () => {
   try {
-    const items = await prisma.item.findMany({
-      include: {
-        _count: true,
-      },
-    });
+    const items = await prisma.item.findMany();
     return NextResponse.json(items, { status: 200 });
   } catch (error) {
     console.error(error);
