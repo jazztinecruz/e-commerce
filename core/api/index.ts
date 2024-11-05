0c53e426fb4bd3681417c27b8fe908da05d24ee3
jazztinecruz <me@jazztinecruz.com>
2024-11-05 17:17:20 +0800

    feat(wishlist): Add API for retrieving wishlist items and update add-item-to-wishlist to use wishlistId

--- core/api/index.ts
+++ core/api/index.ts
@@ -7,6 +7,7 @@ import updateCartItem from "./cart/update-cart-item";
 import type { CartItem as PrismaCartItem } from "@prisma/client";
 import removeCartItem from "./cart/remove-cart-item";
 import addItemToWishlist from "./wishlist/add-item-to-wishlist";
+import getWishlistItems from "./wishlist/get-wishlist-items";
 
 const api = {
   get: {
@@ -23,9 +24,10 @@ const api = {
     addItemToCart: (data: CartItem) => addItemToCart(data),
     updateCartItem: (data: PrismaCartItem) => updateCartItem(data),
     removeCartItem: (id: number) => removeCartItem(id),
-    addItemToWishlist: (data: { userId: string; itemId: number }) =>
+    addItemToWishlist: (data: { wishlistId: number; itemId: number }) =>
       addItemToWishlist(data),
     removeWishlistItem: (id: number) => removeCartItem(id),
+    getWishlistItems: (id: number) => getWishlistItems(id),
   },
 };
 
