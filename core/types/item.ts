fe625386aeca7ad3faff86461e0f9fcbbd0a912f
jazztinecruz <me@jazztinecruz.com>
2024-11-05 17:49:18 +0800

    feat(types): Extend item types to include wishlist items and related structures

--- core/types/item.ts
+++ core/types/item.ts
@@ -1,3 +1,19 @@
-import type { CartItem, Item } from "@prisma/client";
+import type { CartItem, Item, WishlistItems } from "@prisma/client";
 
 export type ExtendedCartItem = CartItem & { item: Item };
+export type ExtendedItem = Item & {
+  cartItems: CartItem[];
+  wishlistItems: WishlistItems[];
+};
+
+export type ExtendedWishlistItem = {
+  id: number;
+  wishlistId: number;
+  item: Item;
+};
+
+export type ExtendedWishlist = {
+  id: number;
+  userId: string;
+  items: ExtendedWishlistItem[];
+};
