6fccbcbd3ffd08cbb3bdac57e007305f25b0428a
jazztinecruz <me@jazztinecruz.com>
2024-11-05 18:15:49 +0800

    feat(wishlist): Enhance Wishlist page to display items and loading state

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,10 +1,27 @@
 import api from "@/core/api";
+import ItemCard from "../_components/items/card";
 
 const Wishlist = async () => {
   const wishlist = await api.get.wishlistItems(1);
 
-  console.log(wishlist);
-  return <div>Wishlist</div>;
+  if (!wishlist) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="space-y-4">
+        <span className="text-sm text-default-500">
+          {wishlist?.items.length} Items Found
+        </span>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {wishlist?.items?.map((item) => (
+            <ItemCard key={item.id} item={item.item} wishlistItemId={item.id} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Wishlist;
